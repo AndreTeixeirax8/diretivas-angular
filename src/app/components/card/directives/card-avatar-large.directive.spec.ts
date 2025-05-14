5f82e4582fe8a1d95295277859f6cd82c77fa525
André Teixeira <andre-x8@hotmail.com>
2025-05-13 21:06:54 -0300

    inicio da aula diretivas

---
+++ src/app/components/card/directives/card-avatar-large.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CardAvatarLargeDirective } from './card-avatar-large.directive';
+
+describe('CardAvatarLargeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CardAvatarLargeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
