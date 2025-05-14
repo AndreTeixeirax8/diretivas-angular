cf18431fa7c1d1e268df04cc1e7f4a484c7ce4b3
André Teixeira <andre-x8@hotmail.com>
2025-05-13 21:32:55 -0300

    ajustes no html

--- src/app/components/card/directives/card-avatar-large.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CardAvatarLargeDirective } from './card-avatar-large.directive';
-
-describe('CardAvatarLargeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CardAvatarLargeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
